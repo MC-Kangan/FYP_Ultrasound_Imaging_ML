ee4c727f54f171934f969e2373819ee2ff5913cf
“MC-Kangan” <kc719@ic.ac.uk>
2023-01-20 10:47:19 +0000

    Remove array from images; reproduce training dataset; model building

--- model_building_2.py
+++ model_building_2.py
@@ -7,7 +7,6 @@ import keras
 from keras.models import Sequential
 from keras.layers import Dense, Flatten, Conv3D, MaxPooling3D, Dropout, BatchNormalization
 
-
 def create_model2():
 
     sample_shape = (895, 16, 16, 1)

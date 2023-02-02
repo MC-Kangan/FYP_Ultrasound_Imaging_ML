cac77b5ce0c7f87a2e2203aefbe6cebcb622f8aa
“MC-Kangan” <kc719@ic.ac.uk>
2023-02-02 13:09:53 +0000

    More successful model development

--- model_building_3d.py
+++ model_building_3d.py
@@ -4,10 +4,7 @@ from tensorflow.keras.models import Sequential
 from keras.layers import Conv3D, MaxPool3D, Flatten, Dense
 from keras.layers import Dropout, Input, BatchNormalization
 from sklearn.metrics import confusion_matrix, accuracy_score
-# from plotly.offline import iplot, init_notebook_mode
-# from keras.losses import categorical_crossentropy
 from keras.optimizers import Adadelta
-# import plotly.graph_objs as go
 from matplotlib.pyplot import cm
 from keras.models import Model
 import pickle
@@ -22,14 +19,14 @@ def create_model_3D(input_size, output_size):
     model.add(Input(input_size))
     # model.add(Dropout(0.2))
     model.add(Conv3D(filters=2, kernel_size=(5, 3, 3), strides=(1, 1, 1), activation='tanh'))
-    # model.add(Dropout(0.1))
-    # model.add(Conv2D(16, (3, 3), activation='tanh'))
-    # model.add(Dropout(0.1))
-
-    model.add()
+    model.add(Dropout(0.1))
+    model.add(MaxPool3D(pool_size=(2, 2, 2), strides=(2, 2, 2)))
+    model.add(Conv3D(filters=2, kernel_size=(5, 3, 3), strides=(1, 1, 1), activation='tanh'))
+    model.add(MaxPool3D(pool_size=(2, 2, 2), strides=(2, 2, 2)))
+    model.add(Dropout(0.1))
 
     model.add(Flatten())
-    model.add(Dense(units=1000, activation='softsign'))
+    model.add(Dense(units=1000, activation='tanh'))
     # model.add(Dropout(0.4))
 
     model.add(Dense(units=output_size, activation='tanh'))
@@ -41,21 +38,21 @@ if __name__ == "__main__":
 
     x_dimension = 3
     img_resize_factor = 50
-    epochs = 10
+    epochs = 100
 
     X, y = load_training_data(num_sample=1000, x_dimension=x_dimension, img_resize_factor=img_resize_factor,
                               shrinkx=True, stack = False)
     input_size, output_size = X.shape[1:], y.shape[1]
 
     modelname = model_namer(dimension=x_dimension, num_sample=len(X), sub_sample=5, fmc_scaler=1.75e-14,
-                            img_resize=img_resize_factor, remark='shrinkX-no-pooling', epochs=epochs, version=2)
+                            img_resize=img_resize_factor, remark='2-3dconv', epochs=epochs, version=3)
 
     print(f'The model name is {modelname}')
     # Build model.
     model = create_model_3D(input_size, output_size)
     print(model.summary())
 
-    callback = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=5)
+    callback = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=100)
 
     # Compile the model
     model.compile(loss='mean_squared_error',

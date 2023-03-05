303c6bf915a20532a98dc6176ab6e1173ec6a6e5
“MC-Kangan” <kc719@ic.ac.uk>
2023-03-05 20:21:52 +0000

    Update the Keras_tuner_gpu.py. Include a global variable that defines the dimension of the CNN.

--- Keras_tuner_gpu.py
+++ Keras_tuner_gpu.py
@@ -13,7 +13,7 @@ from keras_tuner.engine.hyperparameters import HyperParameters
 import keras_tuner as kt
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
-from keras.layers import Conv3D, MaxPool3D, Flatten, Dense, AveragePooling3D
+from keras.layers import Conv3D, MaxPool3D, Flatten, Dense, AveragePooling3D, Conv2D, MaxPool2D, AveragePooling2D
 from keras.layers import Dropout, Input, BatchNormalization
 from sklearn.metrics import confusion_matrix, accuracy_score
 from keras.optimizers import Adadelta
@@ -23,37 +23,62 @@ import numpy as np
 import keras
 import cv2 as cv
 from utility_function import img_resize, model_namer, model_namer_description, save_ml_model, load_ml_model, load_training_data
-from keras import backend as K
 
+# Declare the global variable, CNN dimension: 2 or 3
+CNN_DIM = 2
 
-# config = tf.compat.v1.ConfigProto(intra_op_parallelism_threads=8,
-#                        inter_op_parallelism_threads=8)
-# sess = tf.compat.v1.Session(config=config)
-# K.set_session(sess)
-
+# Declare the global variable, REMARK: usually the version number
+REMARK = 'V1'
 
 def build_model(hp):
-    input_size, output_size = (895, 16, 16, 1), 7200
-    model = Sequential()
-    model.add(Input(input_size))
-    for i in range(hp.Int('layers', 1, 2)):
 
-        model.add(Conv3D(filters=hp.Int('filters_' + str(i), min_value=1, max_value=16, step=1),
-                         kernel_size=(hp.Int('kernels_' + str(i), min_value=3, max_value=5, step=1), 3, 3),
-                         activation=hp.Choice('activation_' + str(i), ["relu", "tanh"])))
+    print(f'The dimension of the CNN model is {CNN_DIM}')
+    if CNN_DIM == 3:
+        input_size, output_size = (895, 16, 16, 1), 7200
 
-        if hp.Boolean("dropout_" + str(i)):
-            model.add(Dropout(rate=0.1))
+        model = Sequential()
+        model.add(Input(input_size))
 
-        if hp.Choice('pooling_' + str(i), ['avg', 'max']) == 'max':
-            model.add(MaxPool3D())
-        else:
-            model.add(AveragePooling3D())
+        for i in range(hp.Int('layers', 1, 2)):
+
+            model.add(Conv3D(filters=hp.Int('filters_' + str(i), min_value=1, max_value=16, step=1),
+                             kernel_size=(hp.Int('kernels_' + str(i), min_value=3, max_value=5, step=1), 3, 3),
+                             activation=hp.Choice('activation_' + str(i), ["relu", "tanh"])))
+
+            if hp.Boolean("dropout_" + str(i)):
+                model.add(Dropout(rate=0.1))
+
+            if hp.Choice('pooling_' + str(i), ['avg', 'max']) == 'max':
+                model.add(MaxPool3D())
+            else:
+                model.add(AveragePooling3D())
+
+    elif CNN_DIM == 2:
+        input_size, output_size = (895, 256, 1), 7200
+
+        model = Sequential()
+        model.add(Input(input_size))
+
+        for i in range(hp.Int('layers', 1, 5)):
+
+
+            model.add(Conv2D(filters=hp.Int('filters_' + str(i), min_value=1, max_value=16, step=1),
+                             kernel_size=(hp.Int('kernels_' + str(i), min_value=3, max_value=5, step=1), 3),
+                             activation=hp.Choice('activation1_' + str(i), ["relu", "tanh"])))
+
+            if hp.Boolean("dropout_" + str(i)):
+                model.add(Dropout(rate=0.1))
+
+            if hp.Choice('pooling_' + str(i), ['avg', 'max']) == 'max':
+                model.add(MaxPool2D())
+            else:
+                model.add(AveragePooling2D())
 
     model.add(Flatten())
     for j in range(hp.Int('dense_layers', 1, 2)):
-        model.add(Dense(units=hp.Int('dense_units_'+str(j), min_value=500, max_value=2000, step=500), activation='softsign'))
-        if hp.Boolean("dropout2_"+ str(j)):
+        model.add(Dense(units=hp.Int('dense_units_'+str(j), min_value=500, max_value=2000, step=500),
+                        activation=hp.Choice('activation2_' + str(j), ["softsign", "tanh"])))
+        if hp.Boolean("dropout2_" + str(j)):
             model.add(Dropout(rate=0.1))
 
     model.add(Dense(units=output_size, activation='tanh'))
@@ -69,30 +94,31 @@ def build_model(hp):
 
 if __name__ == "__main__":
 
-    # Limit TensorFlow to use at most 8 CPU cores
-    # config = tf.compat.v1.ConfigProto(device_count={'CPU': 4})
-
-    # Set the session configuration
-    # sess = tf.compat.v1.Session(config=config)
-
     print('Message 1:')
     print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
     print('______')
     
-    x_dimension = 3
+    x_dimension = CNN_DIM
     img_resize_factor = 50
     epochs = 100
 
-    X, y = load_training_data(num_sample=2000, x_dimension=x_dimension, img_resize_factor=img_resize_factor,
+    X, y = load_training_data(num_sample=100, x_dimension=x_dimension, img_resize_factor=img_resize_factor,
                               shrinkx=False, stack=False)
     input_size, output_size = X.shape[1:], y.shape[1]
 
+    if CNN_DIM == 3:
+        project = f'3dcnn_all_gpu_{REMARK}'
+
+    elif CNN_DIM == 2:
+        project = f'2dcnn_all_gpu_{REMARK}'
+
+
     tuner = kt.Hyperband(build_model,
                          objective=kt.Objective("val_loss", direction="min"),
-                         max_epochs=100,
+                         max_epochs=epochs,
                          factor=3,
                          directory='fyp_model_search',
-                         project_name='3dcnn_1conv_all_gpu_2')
+                         project_name=project)
 
     stop_early = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=10)
 
@@ -112,4 +138,3 @@ if __name__ == "__main__":
 
     result = tuner.results_summary()
 
-    # sess.close()

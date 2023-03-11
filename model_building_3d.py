acdcc53854a5e3b621122e1853c11a3543149a91
“MC-Kangan” <kc719@ic.ac.uk>
2023-03-11 21:05:38 +0000

    Edit the model_building_3d.py to include cross-validation; Also allow the model to train 5 models in the same file.

--- model_building_3d.py
+++ model_building_3d.py
@@ -19,6 +19,7 @@ import pickle
 import numpy as np
 import keras
 import cv2 as cv
+from sklearn.model_selection import KFold
 from utility_function import img_resize, model_namer, model_namer_description, save_ml_model, load_ml_model, load_training_data
 
 def create_model_3D(input_size, output_size, model_num):
@@ -61,21 +62,28 @@ def create_model_3D(input_size, output_size, model_num):
                       loss='mean_squared_error',
                       metrics=[tf.keras.metrics.MeanSquaredError()])
 
-    elif model_num == 4:
-        model = Sequential()
-        model.add(Input(input_size))
-
+    elif model_num == 4: # 3D_2000_o7121864
         model.add(Conv3D(filters=5, kernel_size=(4, 3, 3), strides=(1, 1, 1), activation='tanh'))
         model.add(Dropout(0.1))
-
         model.add(MaxPool3D())
-
         model.add(Flatten())
         model.add(Dense(units=1000, activation='softsign'))
         model.add(Dropout(0.1))
         model.add(Dense(units=output_size, activation='tanh'))
 
         learning_rate = 0.0001735701471314462
+        model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate),
+                      loss='mean_squared_error',
+                      metrics=[tf.keras.metrics.MeanSquaredError()])
+
+    elif model_num == 5: # 3D_2000_o7127128-2
+        model.add(Conv3D(filters=4, kernel_size=(3, 3, 3), strides=(1, 1, 1), activation='tanh'))
+        model.add(Dropout(0.1))
+        model.add(MaxPool3D())
+        model.add(Flatten())
+        model.add(Dense(units=1000, activation='softsign'))
+        model.add(Dense(units=output_size, activation='tanh'))
+        learning_rate = 0.0001960411264334168
         model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate),
                       loss='mean_squared_error',
                       metrics=[tf.keras.metrics.MeanSquaredError()])
@@ -92,32 +100,73 @@ if __name__ == "__main__":
     X, y = load_training_data(num_sample=10, x_dimension=x_dimension, img_resize_factor=img_resize_factor,
                               shrinkx=False, stack=False)
 
-    model_num_list = [1,2,3,4]
+    print('')
+
+    model_num_list = [5]
     model_name_dict = {1: '3D_2450_o7167535',
                        2: '3D_2000_o7158970',
                        3: '3D_2000_o7127128',
-                       4: '3D_2000_o7121864'}
+                       4: '3D_2000_o7121864',
+                       5: '3D_2000_o7127128-2'}
 
     for i in model_num_list:
         modelname = model_name_dict[i]
-        print(f'The model name is {modelname}')
+        print(f'Model {i}: The model name is {modelname}')
 
         # Build model.
         input_size, output_size = X.shape[1:], y.shape[1]
         print(f'Input_size: {input_size}; Output_size: {output_size}')
 
-        model = create_model_3D(input_size, output_size, model_num = i)
-        print(model.summary())
+        num_folds = 5
 
-        callback = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=100)
+        # Define the K-fold Cross Validator
+        kfold = KFold(n_splits=num_folds, shuffle=True)
 
-        # Fit data to model
-        history = model.fit(X, y,
-                            batch_size=32,
-                            epochs=epochs,
-                            verbose=1,
-                            validation_split=0.2,
-                            callbacks=[callback],
-                            workers=NCPUS)
+        # K-fold Cross Validation model evaluation
+        fold_no = 1
 
+        loss_lst, val_loss_lst = [], []
+
+        for train, test in kfold.split(X, y):
+            print(f'train_size: {X[train].shape}; test_size: {y[train].shape}')
+
+            model = create_model_3D(input_size, output_size, model_num = i)
+            # print(model.summary())
+
+            callback = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=100)
+
+            # Generate a print
+            print('------------------------------------------------------------------------')
+            print(f'Training for fold {fold_no} ...')
+
+            # Fit data to model
+            train_history = model.fit(X[train], y[train],
+                                batch_size=32,
+                                epochs=epochs,
+                                verbose=1,
+                                validation_split=0.2,
+                                callbacks=[callback],
+                                workers=NCPUS)
+
+            # Generate generalization metrics
+            scores = model.evaluate(X[test], y[test], verbose=0)
+            loss = train_history.history['loss'][0]
+            val_loss = train_history.history['val_loss'][0]
+
+            loss_lst.append(loss)
+            val_loss_lst.append(val_loss)
+            print(f'Score_for_fold_{fold_no} - loss: {loss} - val_loss: {val_loss}')
+
+            # Increase fold number
+            fold_no = fold_no + 1
+
+        print(f'Model_{modelname} - average_loss: {np.mean(loss_lst)} - average_val_loss: {np.mean(val_loss_lst)}')
+        print('') # print an empty line
+
+        # Save the model
         # save_ml_model(model, modelname)
+
+
+
+
+
--- working_script.py
+++ working_script.py
@@ -1,50 +1,52 @@
-import tensorflow as tf
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
+from tensorflow.keras.losses import sparse_categorical_crossentropy
+from tensorflow.keras.optimizers import Adam
+from sklearn.model_selection import KFold
+import numpy as np
 
-print('Message 1:')
-print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
-print('______')
+num_folds = 5
 
+# Define the K-fold Cross Validator
+kfold = KFold(n_splits=num_folds, shuffle=True)
 
-print('Message 2:')
-strategy = tf.distribute.MirroredStrategy()
-print("Number of devices: {}".format(strategy.num_replicas_in_sync))
-print('______')
+# K-fold Cross Validation model evaluation
+fold_no = 1
+for train, test in kfold.split(inputs, targets):
 
-print('Message 3:')
-tf.debugging.set_log_device_placement(True)
+    # Define the model architecture
+    model = Sequential()
+    model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Flatten())
+    model.add(Dense(256, activation='relu'))
+    model.add(Dense(128, activation='relu'))
+    model.add(Dense(no_classes, activation='softmax'))
 
-# Create some tensors
-a = tf.constant([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
-b = tf.constant([[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]])
-c = tf.matmul(a, b)
-
-print(c)
-print('______')
-
-print('Message 4:')
-if tf.test.gpu_device_name():
-    print('Default GPU Device: {}'.format(tf.test.gpu_device_name()))
-else:
-    print('Please install GPU version of TF')
-print('______')
-
-print('Message 5:')
-gpus = tf.config.list_physical_devices('GPU')
-print(f'tf.config.list_physical_devices(GPU): {gpus}')
-print('______')
-
-print('Message 6:')
-if gpus:
-  # Restrict TensorFlow to only use the first GPU
-  try:
-    tf.config.set_visible_devices(gpus[0], 'GPU')
-    logical_gpus = tf.config.list_logical_devices('GPU')
-    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPU")
-  except RuntimeError as e:
-    # Visible devices must be set before GPUs have been initialized
-    print(e)
+    # Compile the model
+    model.compile(loss=loss_function,
+                optimizer=optimizer,
+                metrics=['accuracy'])
 
 
+    # Generate a print
+    print('------------------------------------------------------------------------')
+    print(f'Training for fold {fold_no} ...')
 
+    # Fit data to model
+    history = model.fit(inputs[train], targets[train],
+              batch_size=batch_size,
+              epochs=no_epochs,
+              verbose=verbosity)
 
+    # Generate generalization metrics
+    scores = model.evaluate(inputs[test], targets[test], verbose=0)
+    print(f'Score for fold {fold_no}: {model.metrics_names[0]} of {scores[0]}; {model.metrics_names[1]} of {scores[1]*100}%')
+    acc_per_fold.append(scores[1] * 100)
+    loss_per_fold.append(scores[0])
 
+    # Increase fold number
+    fold_no = fold_no + 1
